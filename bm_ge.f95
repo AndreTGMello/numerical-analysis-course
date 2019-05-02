e0483562043a0885a1a87ca0a37b354d27424fbd
AndreTGMello <andre.tgmello@gmail.com>
2019-05-02 19:02:20 -0300

    Organizing code. Dynamic matrix.

--- bm_ge.f95
+++ bm_ge.f95
@@ -1,18 +1,31 @@
 program bm_ge
   ! Program for banded matrix gauss elimination
-  use Utils
+  use utils
 
   implicit none
 
-  real, dimension(3,3) :: matrix
-  integer :: i,j
+  !---------------
+  ! Variables
+  !---------------
+  real, dimension(:,:), allocatable :: matrix
+  integer :: i,j,m,n,k
 
-  do i=1,3
-    do j=1,3
+  !---------------
+  ! Procedures
+  !---------------
+  print *, "Enter square matrix number of rows/columns:"
+  read *, m
+  print *, "Enter number of diagonals:"
+  read *, n
+  allocate(matrix(m,n))
+
+  do i=1,m
+    do j=1,n
       matrix(i,j)=i+j
     end do
   end do
 
-  pprint_mat(matrix)
+  call pprint_mat(matrix,m,n)
 
+  deallocate(matrix)
 end program bm_ge
