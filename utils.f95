25e83077a5eff4d792037fbd54ca95ae796442a7
AndreTGMello <andre.tgmello@gmail.com>
2019-05-03 12:56:26 -0300

    char(10) is \n

--- utils.f95
+++ utils.f95
@@ -7,10 +7,11 @@ contains
 		real, dimension(m,n) :: matrix
 
 		! n = shape(M)
-
+		print *, "!-------------------"
 		do i=lbound(matrix,1),ubound(matrix,1)
 			write(*,*) (matrix(i,j), j=lbound(matrix,2), ubound(matrix,2))
 		end do
+		print *, "!-------------------",char(10)
 	end subroutine
 
 end module utils
