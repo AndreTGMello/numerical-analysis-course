b15ca20da7205e2323db1ccf73851aa262420d50
André Mello <andre.tgmello@gmail.com>
2019-05-13 00:46:45 -0300

    New data structure for banded matrix

--- main.f95
+++ main.f95
@@ -4,21 +4,50 @@ use utils
 
 implicit none
 
+type diagonal
+    real, allocatable :: d(:)
+endtype diagonal
+
 !---------------
 ! Variables
 !---------------
-integer :: i,j,k,l,m,n
-real, dimension(5) :: b
-real, dimension(:,:), allocatable :: A
+
+integer :: i,j,k,l,m,n,ub,lb
+real :: b(5)
+type(diagonal), allocatable :: D(:)
 
 !---------------
 ! Logic
 !---------------
-k = 5
-m = 6
-allocate(A(m,k))
-A = reshape((/0,0,8,13,18,22,0,4,9,14,19,23,1,5,10,15,20,24,2,6,11,16,21,0,3,7,12,17,0,0/), shape(A))
 
-call pprint_mat(A,m,k)
-deallocate(A)
+! ub -> upper band
+! lb -> lower band
+ub = 2
+lb = 2
+k = ub+lb+1
+m = 6
+
+allocate(D(k))
+
+! Allocate upper band diagonals
+do i = ub, 1, -1
+    allocate(D(i)%d(m-i))
+end do
+! Allocate main diagonal (i = j)
+allocate(D(ub+1)%d(m))
+! Allocate lower band diagonals
+do i = 1, lb
+    allocate(D(ub+1+i)%d(m-i))
+end do
+
+do i = 1, size(D, dim=1)
+	print *, "!-------------------",char(10)
+    do j = 1, size(D(i)%d(:))
+        print *, D(i)%d(j)
+    end do
+	print *, "!-------------------",char(10)
+end do
+
+!call pprint_mat(A,m,k)
+deallocate(D)
 end program main
