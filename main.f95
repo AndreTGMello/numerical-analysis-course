51d8eeddf01042870c9107b7218f8c9022f463c4
André Mello <andre.tgmello@gmail.com>
2019-05-18 16:32:51 -0300

    New data structure for banded matrix. Gaussian elimination first sketch

--- main.f95
+++ main.f95
@@ -13,7 +13,7 @@ endtype diagonal
 !---------------
 
 integer :: i,j,k,l,m,n,ub,lb
-real :: b(5)
+real :: b(5), multiplier
 type(diagonal), allocatable :: D(:)
 
 !---------------
@@ -24,20 +24,20 @@ type(diagonal), allocatable :: D(:)
 ! lb -> lower band
 ub = 2
 lb = 2
-k = ub+lb+1
-m = 6
+! Square matrix size
+m = 4
 
-allocate(D(k))
+allocate(D(1+lb+ub))
 
-! Allocate upper band diagonals
-do i = ub, 1, -1
-    allocate(D(i)%d(m-i))
-end do
 ! Allocate main diagonal (i = j)
-allocate(D(ub+1)%d(m))
+allocate(D(1)%d(m))
 ! Allocate lower band diagonals
 do i = 1, lb
-    allocate(D(ub+1+i)%d(m-i))
+    allocate(D(i+1)%d(m-i))
+end do
+! Allocate upper band diagonals
+do i = 1, ub
+    allocate(D(lb+1+i)%d(m-i))
 end do
 
 do i = 1, size(D, dim=1)
@@ -48,6 +48,23 @@ do i = 1, size(D, dim=1)
 	print *, "!-------------------",char(10)
 end do
 
+! Gauss Elimination
+do i = 1, m-1
+    ! For each lower diagonal, do:
+    do j = 2, lb+1
+        multiplier = D(j)%d(i)/D(1)%d(i)
+        D(j)%d(i) = 0
+        ! For each upper diagonal, subtract from the diagonal below:
+        do k = 1+lb+1, 1+lb+ub
+            D(mod(k-1,lb+1))%(i+1) = D(mod())%d(i+1) - multiplier*D(k)%d(i)
+        end do
+    end do
+end do
+
 !call pprint_mat(A,m,k)
+do i=1, size(D)
+    deallocate(D(i)%d)
+end do
 deallocate(D)
+
 end program main
