462108ce6f8ca04b556557236cdd5862140b9159
André Mello <andre.tgmello@gmail.com>
2019-06-09 21:55:38 -0300

    Add bspline basis algorithm
    Module for bspline contains highly specialized routines for
    calculating both linear and cubic spline basis.
    
    Signed-off-by: Andre Mello <andre.tgmello@gmail.com>

--- bspline.f95
+++ bspline.f95
@@ -4,42 +4,75 @@ module bspline
   !---------------
   ! Variables
   !---------------
-!  integer :: i,j
+  !  integer :: i,j
 
   !---------------
   ! Logic
   !---------------
   contains
+    function linear_spline(y,x)
+      !---------------
+      ! Variables
+      !---------------
+      ! y are the three points on top of which
+      ! the linear spline sits. Everywhere else
+      ! it is zero.
+      ! x is used for computing the desired value
+      ! this spline assumes in this given point.
+      real, intent(in) :: y(3),x
+      real :: h
+      real :: linear_spline
+      h = y(2) - y(1)
+      linear_spline = 0
+
+      if ( x < y(1) ) then
+        linear_spline = 0
+      elseif ( x <= y(2) ) then
+        linear_spline = (x - y(1))/h
+      elseif ( x <= y(3) ) then
+        linear_spline =  (y(3) - x)/h
+      end if
+    end function linear_spline
+
     function cubic_spline(y, x)
       !---------------
       ! Variables
       !---------------
-      ! y is the partition
-      ! x are the function values at y
-      ! b is the values for the spline
-      ! y_x is the extended partition
-      ! h is the step size, fixed in this case
-      real, allocatable, intent(in) :: y(:), x(:)
-      real, allocatable, intent(out) :: b(:)
-      real, allocatable :: y_x(:)
+      ! y are the five points on top of which
+      ! the cubi spline sits. Everywhere else
+      ! it is zero.
+      ! x is used for computing the desired value
+      ! this spline assumes in this given point.
+      real, intent(in) :: y(5), x
       real :: h
       integer :: i,j
+      real :: cubic_spline
+
+      cubic_spline = 0
 
       h = y(2) - y(1)
+      if ( x < y(1) .or. x > y(5)) then
+        cubic_spline = 0
 
-      ! Build extended partition y_x (y_extended)
-      allocate(y_x(size(y)+2*3))
-      y_x(1) = y(1) - 3*h
-      do i = 2, 2*3+size(y)
-        y_x(i) = y_x(i) + y_x(1)*i*h
-      end do
+      elseif ( x >= y(1) .and. x < y(2) ) then
+        cubic_spline = ((x-y(1))**3)/6*h**3
 
-      allocate(b(size(y_x)))
-      do j = 1, 4
-        do i = 1, size(b)
+      elseif ( x >= y(2) .and. x < y(3) ) then
+        cubic_spline = &
+        ( ((x-y(1))**2)*(y(3)-x) &
+            + (x-y(1))*(y(4)-x)*(x-y(2))   &
+        + ( (y(5)-x)*(x-y(2))**2 ) )/6*h**3
 
-        end do
-      end do
+      elseif ( x >= y(3) .and. x < y(4) ) then
+        cubic_spline = &
+          ( (y(5)-x)*(x-y(2))*(y(4)-x) &
+            + ((y(5)-x)**2)*(x-y(3))  &
+        + (x-y(1))*(y(4)-x)**2 )/6*h**3
+
+      elseif ( x >= y(4) .and. x < y(5) ) then
+        cubic_spline = ((y(5)-x)**3)/6*h**3
+
+      end if
 
     end function cubic_spline
 
