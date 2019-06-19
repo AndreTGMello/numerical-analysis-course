4de925423714123151d5e1963f55cb59257df94d
André Mello <andre.tgmello@gmail.com>
2019-06-19 08:12:22 -0300

    Fix gaussian elimination on banded matrices
    Previous algorithm worked only for a specific upper and lower bands
    size. Algorithm should work on more general banded matrices now.
    
    Signed-off-by: Andre Mello <andre.tgmello@gmail.com>

--- g_elimination.f95
+++ g_elimination.f95
@@ -29,7 +29,7 @@ contains
   end subroutine
 
   subroutine gaussian_elimination_banded(Row, x, b, lb, ub)
-    integer :: i,j,k,l,m,n,ms
+    integer :: i,j,k,l,m,n,ms,fix_col_index,fix_diag_col_index,temp_fix_col_index
     integer, intent(in) :: lb, ub
     real(wp), allocatable, intent(inout) :: x(:), b(:)
     type(col), allocatable, intent(inout) :: Row(:)
@@ -37,56 +37,73 @@ contains
     ! Matrix Size:
     ms = size(Row)
 
+    fix_col_index = 0
+    fix_diag_col_index = 0
     ! Gaussian Elimination
     do i = 1, ms-1
         ! For each row:
         j = i + 1
+
+        ! Fix pivot index
+        if ( i > (lb+1) ) then
+          fix_diag_col_index = fix_diag_col_index + 1
+        end if
+
         do while ( j <= i+lb .and. j <= ms)
-            l = 0
-            m = 0
-            ! If row is below the number of lower + uper bands,
-            ! adjust index
-            if ( i > lb+1 ) then
-                l = ms - size(Row(i)%Col)
-                m = ms - size(Row(j)%Col)
-            elseif ( j > lb+1 ) then
-                m = ms - size(Row(j)%Col)
-            end if
-            Row(j)%Col(i-m) = Row(j)%Col(i-m)/Row(i)%Col(i-l)
-            b(j) = b(j) - Row(j)%Col(i-m)*b(i)
-            ! For each column:
-            k = i + 1
-            do while ( k <= i+ub .and. k <= ms )
-                Row(j)%Col(k-m) = Row(j)%Col(k-m) - Row(j)%Col(i-m)*Row(i)%Col(k-l)
-                k = k + 1
-            end do
-            Row(j)%Col(i-m) = 0
-            j = j + 1
+
+          ! Fix column index
+          if ( j > (lb+1) ) then
+            fix_col_index = j - (lb+1)
+          else
+            fix_col_index = 0
+          end if
+
+          ! Multiplier
+          Row(j)%Col(i-fix_col_index) = Row(j)%Col(i-fix_col_index)&
+                                        /Row(i)%Col(i-fix_diag_col_index)
+
+          ! Apply multiplier to the systems solution (b)
+          b(j) = b(j) - Row(j)%Col(i-fix_col_index)*b(i)
+
+          ! For each column:
+          k = i + 1
+          do while ( k <= i+ub .and. k <= ms )
+              Row(j)%Col(k-fix_col_index) = Row(j)%Col(k-fix_col_index)&
+              - Row(i)%Col(k-fix_diag_col_index)*Row(j)%Col(i-fix_col_index)
+              k = k + 1
+          end do
+
+          ! Set multiplier to zero
+          Row(j)%Col(i-fix_col_index) = 0
+
+          j = j + 1
         end do
+!        call pprint_band(Row,lb,ub)
     end do
+
 
     ! Solving the system
     ! k and l are auxiliary variables for index fixing/shifting
     ! due to the way the band matrix is stored.
     l = ub + 1
+
     do i = ms, ms-ub, -1
         x(i) = b(i)
         k = ms
         do j = size(Row(i)%Col, dim=1), size(Row(i)%Col, dim=1) - ub + l, -1
             x(i) = x(i) - Row(i)%Col(j)*x(k)
             k = k-1
-            write(*,*)
         end do
         x(i) = x(i)/Row(i)%Col(j)
         l = l - 1
     end do
+
     do i = ms - ub - 1, 1, -1
         x(i) = b(i)
         k =  size(Row(i)%Col)
         do j = size(Row(i)%Col), size(Row(i)%Col) - ub + 1, -1
             x(i) = x(i) - Row(i)%Col(j)*x(k)
             k = k-1
-            write(*,*)
         end do
         x(i) = x(i)/Row(i)%Col(j)
     end do
